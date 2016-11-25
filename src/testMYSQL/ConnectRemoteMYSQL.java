6023ac1555202c68e670cc8c5e8959f1d2edfd30
bartvdt <bart.van.der.tang@sprintf.nl>
2016-11-25 12:43:32 +0100

    Corrected print statement

--- src/testMYSQL/ConnectRemoteMYSQL.java
+++ src/testMYSQL/ConnectRemoteMYSQL.java
@@ -7,7 +7,7 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.Properties;
 
-import org.omg.PortableInterceptor.SYSTEM_EXCEPTION;
+//import org.omg.PortableInterceptor.SYSTEM_EXCEPTION;
 
 import java.sql.Statement;
 import java.sql.PreparedStatement;
